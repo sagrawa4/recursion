7b4368982dbf95c12e4e56eedaf7d3d953ebe111
Shruti Agrawal <shruti98932@gmail.com>
2021-06-05 13:50:44 -0400

    implemented using element.classList and children

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,43 +4,19 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
-
-  //console.log(document.body);
-
-  //console.log(document.body.classList);
-  //DOMTokenList ["targetClassName", value: "targetClassName"]
-
-  //console.log(document.body.children);
-  //HTMLCollection(2) [div#mocha, div.targetClassName, mocha: div#mocha]
-
-  //console.log(document.body.childNodes);
-   //NodeList(4) [text, div#mocha, text, div.targetClassName]
-
-   //console.log(document.body.classList);
-  //console.log(document.body.childNodes[1]);
-  // //NodeList(4) [text, div#mocha, text, div.targetClassName]
-  //console.log(document.body.childNodes[3]);
-
-  //console.log(document.body.children[1].className);
-
+var getElementsByClassName = function(className) {
   var resultArr = [];
-  resultArr.push(document.body);
-
-for(var i=0; i<document.body.children.length;i++) {
-      if(document.body.children[i].className) {
-        resultArr.push(document.body.children[i]);
-      } else {
-      for(var j=0; j< document.body.children[i].children.length; j++) {
-          if(document.body.children[i].children[j].className) {
-            resultArr.push(document.body.children[i].children[j]);
-        }
+  var element = document.body;
+  var innerfunction = function(element) {
+   if (element.classList.contains(className)){
+      resultArr.push(element);
+    }
+    if (element.children){
+      for (var i = 0; i < element.children.length; i++){
+        innerfunction(element.children[i]);
       }
-  }
-}
-
-return resultArr;
-
+    }
+  };
+  innerfunction(element);
+  return resultArr;
 };
